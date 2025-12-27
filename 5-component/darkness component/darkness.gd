25b3d3965ec71561df12c97cf73e41eb44670a8c
Jakob Bennie <jayfeather874@gmail.com>
2025-12-27 12:58:34 -0500

    Added lighting and changed some sprites

---
+++ 5-component/darkness component/darkness.gd
@@ -0,0 +1,1 @@
+extends DirectionalLight2D
---
+++ 5-component/light component/light.gd
@@ -0,0 +1,22 @@
+extends PointLight2D
+
+@export var flickerMin: float = 0.9
+@export var flickerMax: float = 1.0
+@export var baseFlickerTime: float = 1.0
+@export var rangeMin: float = 450
+@export var rangeMax: float = 400
+
+var currentEnergy: float = randf_range(flickerMin, flickerMax)
+var wantedEnergy: float = randf_range(flickerMin, flickerMax)
+var flickerTime: float = 0.0
+var flickerIterator: float = 0.0
+
+
+func _process(delta):
+	flickerTime -= delta
+	self.energy += flickerIterator * delta
+	if flickerTime <= 0:
+		flickerTime = baseFlickerTime
+		wantedEnergy = randf_range(flickerMin, flickerMax)
+		flickerIterator = (wantedEnergy - self.energy) / baseFlickerTime
+		currentEnergy = self.energy

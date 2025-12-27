521cd7cd1979165162f5b72008f6a8e838421b91
Jakob Bennie <jayfeather874@gmail.com>
2025-12-26 20:59:58 -0500

    Monkey Fix

--- 4-objects/1-npc/npc.gd
+++ 4-objects/1-npc/npc.gd
@@ -6,6 +6,9 @@ var characterID: Data.Characters
 var talked: Dictionary # which mask they've talked to
 var susLevel: int
 
+var moveDirection: Vector2
+var wanderTime: float
+
 @export var mapSprite: Texture2D
 @export var dialogueSprite: Texture2D
 
@@ -27,7 +30,22 @@ func _init(characterID: Data.Characters, npcname: String, susLevel: int):
 	
 	generateDialogueTrees()
 	CLogger.info("Initialized %s" % npcname)
+
+func _ready():
+	roamBuilding()
+
+func roamBuilding():
+	# I would want some sort of check to see if they NPC is going to a spot within the world border AND not intersecting with a wall
+	# We can work that out later
 	
+	var randX = randi_range(0, 512)
+	var randY = randi_range(0, 512)
+	CLogger.debug("Normalized Version = " + str(Vector2(randX, randY).normalized()))
+	CLogger.debug("Regular Version = " + str(Vector2(randX, randY)))
+	moveDirection = Vector2(randX, randY)
+	
+	
+
 func generateDialogueTrees():
 	var n: int = len(Data.PlayerMasks)
 	for i in range(n):

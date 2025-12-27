68dfdfa1e0ff5f59152cbc88ebf0706dae384551
Jakob Bennie <jayfeather874@gmail.com>
2025-12-26 19:56:10 -0500

    Some additions

---
+++ 4-objects/0-character/character.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+
+var currState = STATE.MOVING
+
+enum STATE {
+	MOVING,
+	TALKING
+}
+
+
+func _ready() -> void:
+	pass
+	# initialize talked to array
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if currState == STATE.MOVING:
+		processMovement()
+		
+	
+
+func processMovement():
+	var movement_vector: Vector2 = get_movement_vector()
+	var direction: Vector2 = movement_vector.normalized()
+	self.velocity = direction * self.BASE_SPEED
+	self.move_and_slide()
+	pass
+	
+func get_movement_vector() -> Vector2:
+	var x_movement = Input.get_action_strength("right") - Input.get_action_strength("left")
+	var y_movement = Input.get_action_strength("down") - Input.get_action_strength("up")
+	return Vector2(x_movement, y_movement)

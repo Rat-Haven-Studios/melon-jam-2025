b1bd8a97f1de2c09ba4bf8eb8b65ff79f63fc1ac
Jakob Bennie <jayfeather874@gmail.com>
2025-12-26 20:10:26 -0500

    Added the level
    just added the level

---
+++ 4-objects/2-level/camera_2d.gd
@@ -0,0 +1,18 @@
+extends Camera2D
+
+
+var player = null
+
+func _ready():
+	findPlayer()
+
+func _process(float):
+	if player == null:
+		CLogger.debug("Haven't found the player man")
+		findPlayer()
+	else:
+		self.position = player.position
+func findPlayer():
+	CLogger.info("Camera finding player")
+	player = get_tree().get_first_node_in_group("Player")
+	

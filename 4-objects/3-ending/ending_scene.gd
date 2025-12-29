3c20185959001c277b05fd70a01e7cd381933db8
Hunter Baker <bakerhb@dukes.jmu.edu>
2025-12-28 22:59:59 -0500

    game loop implemented

--- 4-objects/3-ending/ending_scene.gd
+++ 4-objects/3-ending/ending_scene.gd
@@ -2,6 +2,14 @@ extends Node2D
 
 const killerID = Data.Characters.THE_HARE
 
+@onready var menuBtn: Button = $CanvasLayer/MarginContainer/VBoxContainer/MarginContainer/HBoxContainer/Playagain
+@onready var quitBtn: Button = $CanvasLayer/MarginContainer/VBoxContainer/MarginContainer/HBoxContainer/quit
+@onready var label: Label = $CanvasLayer/MarginContainer/VBoxContainer/Label2
+
+const MAIN_MENU: String = "res://1-main/Main.tscn"
+const FIRST_LEVEL: String = "res://4-objects/2-level/FIRST_LEVEL.tscn"
+
+
 func _ready() -> void:
 	var whoDied = Data.killed
 	if whoDied == null:
@@ -10,5 +18,16 @@ func _ready() -> void:
 	
 	if whoDied != killerID:
 		CLogger.info("Player selected the wrong person")
+		label.text = "You selected wrong..."
 	else:
 		CLogger.info("Player won!")
+		label.text = "You correctly identified and killed the traitor!"
+
+	menuBtn.pressed.connect(onMenuBtnPressed)
+	quitBtn.pressed.connect(onQuitBtnPressed)
+
+func onMenuBtnPressed():
+	SceneTransitioner.change_scene(MAIN_MENU)
+	
+func onQuitBtnPressed():
+	get_tree().quit()
